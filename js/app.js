bda68382a78bf0dcf94536cde31c97e71d4cdbeb
Ryan Crichton <ryan@jembi.org>
2016-05-26 15:58:13 +0200

    Add DHIS2 menu to app

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 'use strict'
+/* global window */
 
 var angular = require('angular')
 require('angular-material')
 require('angular-route')
 
+var deps = [ 'ngRoute', 'ngMaterial' ]
+// if this is running in dhis2
+if (window.location.href.indexOf('api/apps') >= 0) {
+  deps.push('d2HeaderBar')
+}
 // setup app
-var app = angular.module('dhisOrgUnitApp', [ 'ngRoute', 'ngMaterial' ])
+var app = angular.module('dhisOrgUnitApp', deps)
 
 // register controllers - see controllers/index.js
 require('./controllers')
