ccadd634c654e9a42dbdc105ee43ac94f7b80b51
hnnesv <venterh@gmail.com>
2016-05-31 15:39:46 +0200

    DAT-16: Add support for multiple root orgunits. Preload one level ahead Minor edits and improvements
    PR #2

--- js/controllers/hierarchy.js
+++ js/controllers/hierarchy.js
@@ -7,12 +7,7 @@ module.exports = function ($scope, Api) {
 
   var loadRootOrgUnit = function() {
     var onSuccess = function(result) {
-      if (result.organisationUnits.length > 0) {
-        $scope.root = result.organisationUnits[0]
-        $scope.root.displayName = $scope.root.name
-      } else {
-        $scope.root = null
-      }
+      $scope.roots = result.organisationUnits
     }
 
     Api.OrgUnitRoot.get(onSuccess, errorHandler)
--- js/services/rest.js
+++ js/services/rest.js
@@ -2,7 +2,7 @@
 
 module.exports = function($rootScope, $resource) {
   return {
-    OrgUnitRoot: $resource('/api/organisationUnits.json?paging=false&level=1'),
-    OrgUnitChildren: $resource('/api/organisationUnits/:uid/children.json?fields=id,displayName,displayShortName', { uid: '@_uid' })
+    OrgUnitRoot: $resource('/api/organisationUnits.json?paging=false&level=1&fields=id,name'),
+    OrgUnitChildren: $resource('/api/organisationUnits/:uid/children.json?fields=id,name', { uid: '@_uid' })
   }
 }
